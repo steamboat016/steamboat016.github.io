a9e293f89faa107b891e507bbf8f62de42c00e06
steamboat016 <a2450438770@163.com>
2026-02-28 01:05:16 +0800

    change banner

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "steamboat016 的个人网站",
-	subtitle: "",
+	title: "steamboat016",
+	subtitle: "Personal Website",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/ShirahaneSuoh.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/DITF.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
